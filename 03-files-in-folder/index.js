59aba8292dc4a8ad1d85da72884631b83d27695c
ku4a <7916884@gmail.com>
2023-05-07 01:13:54 +0300

    feat: add task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const output = async () => {
+  const dirPath = path.join(__dirname, 'secret-folder');
+  try {
+    const filesStack = await fs.promises.readdir(dirPath);
+    for (let file of filesStack){
+      let extName = path.extname(file);
+      let fileName = path.basename(file, extName);
+      fs.stat(path.join(__dirname, 'secret-folder', path.basename(file)), (error, stats)=>{
+        if (error) {
+          throw error;
+        }
+        if (stats.isFile()){
+          console.log(`${fileName}-${extName.slice(1)}-${stats.size / 1000}kb`);
+        }
+      });
+    }
+  } catch (error){
+    console.error(error);
+  }
+};
+output();
